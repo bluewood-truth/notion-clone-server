27805324a0da8c5a5cb3bb3f3eaa75e791ccade4
bluewood <bluewood.truth@gmail.com>
2022-06-21 23:19:08 +0900

    TEST: update tests with adding dto

--- src/notions/notions.service.spec.ts
+++ src/notions/notions.service.spec.ts
@@ -1,4 +1,3 @@
-import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { NotionsService } from './notions.service';
 import { NotionsStore } from './store/notions.store';
@@ -20,59 +19,70 @@ describe('NotionsService', () => {
 
   describe('getAllNotions', () => {
     it('should return an array', async () => {
-      const result = await service.getAllNotions();
-      expect(result).toBeInstanceOf(Array);
+      const { notions } = await service.getAllNotions();
+      expect(notions).toBeInstanceOf(Array);
     });
   });
 
   describe('createNotion', () => {
     it('should create a notion', async () => {
-      const beforeCreate = await service.getAllNotions();
-      await service.createNotion('testOwner');
-      const afterCreate = await service.getAllNotions();
+      const beforeCreate = (await service.getAllNotions()).notions;
+      await service.createNotion({ owner: 'testOwner' });
+      const afterCreate = (await service.getAllNotions()).notions;
       expect(afterCreate.length).toBeGreaterThan(beforeCreate.length);
     });
   });
 
   describe('getNotion', () => {
     it('should get a notion', async () => {
-      const created = await service.createNotion('testOwner');
-      const notion = await service.getNotion(created.id);
+      const { notion: created } = await service.createNotion({
+        owner: 'testOwner',
+      });
+      const { notion } = await service.getNotion({ id: created.id });
       expect(notion.id).toBe(created.id);
     });
-    it('should throw a NotFoundException', async () => {
-      try {
-        await service.getNotion('invaild-id');
-      } catch (e) {
-        expect(e).toBeInstanceOf(NotFoundException);
-      }
+
+    it('should send an error', async () => {
+      const { error } = await service.getNotion({ id: 'invaild-id' });
+      expect(error).toBe(true);
     });
   });
 
   describe('deleteNotion', () => {
     it('should delete a notion', async () => {
-      const created = await service.createNotion('testOwner');
-      const beforeDelete = await service.getAllNotions();
-      await service.deleteNotion(created.id);
-      const afterDelete = await service.getAllNotions();
+      const { notion: created } = await service.createNotion({
+        owner: 'testOwner',
+      });
+      const beforeDelete = (await service.getAllNotions()).notions;
+      await service.deleteNotion({ id: created.id });
+      const afterDelete = (await service.getAllNotions()).notions;
       expect(afterDelete.length).toBeLessThan(beforeDelete.length);
     });
-    it('should throw a NotFoundException', async () => {
-      try {
-        await service.deleteNotion('invaild-id');
-      } catch (e) {
-        expect(e).toBeInstanceOf(NotFoundException);
-      }
+
+    it('should send an error', async () => {
+      const { error } = await service.deleteNotion({ id: 'invaild-id' });
+      expect(error).toBe(true);
     });
   });
 
-  describe('editNotionTitle', () => {
+  describe('editNotion', () => {
+    const newTitle = 'Edited title';
+
     it('should edit a title of a notion', async () => {
-      const newTitle = 'Edited title';
-      const created = await service.createNotion('testOwner');
-      await service.editNotionTitle(created.id, newTitle);
-      const edited = await service.getNotion(created.id);
+      const { notion: created } = await service.createNotion({
+        owner: 'testOwner',
+      });
+      await service.editNotion({ notionId: created.id, title: newTitle });
+      const { notion: edited } = await service.getNotion({ id: created.id });
       expect(edited.title).toBe(newTitle);
+    });
+
+    it('should send an error', async () => {
+      const { error } = await service.editNotion({
+        notionId: 'invaild-id',
+        title: newTitle,
+      });
+      expect(error).toBe(true);
     });
   });
 });
