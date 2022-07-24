bd1d769c2595e5d4bb92a490554f9f3699d87370
bluewood <bluewood.truth@gmail.com>
2022-07-24 23:39:30 +0900

    ✨FEAT: add GET controller

--- src/blocks/blocks.controller.ts
+++ src/blocks/blocks.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { Block } from './entities/block.entity';
+
+const dummyBlocks: Block[] = [];
 
 @Controller('blocks')
-export class BlocksController {}
+export class BlocksController {
+  @Get()
+  getAll(): Block[] {
+    return dummyBlocks;
+  }
+
+  @Get(':id')
+  getOne(@Param('id') id: string): Block {
+    return dummyBlocks.find((block) => block.id === id);
+  }
+}
